256b93dedf0e60dfd215365d438c9b78d50bf690
manh3141 <manh3141@users.noreply.github.com>
2018-04-09 14:16:42 +0200

    Add files via upload

---
+++ digital.hhz.de/learnmore.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="_css/styles.css">
+		<title>Digital HHZ - Access sensor values</title>
+	</head>
+	
+	<body>
+		
+		<!-- Include the header bar -->
+		<?php include("header.learnmore.inc.php"); ?>
+		
+		<div class="wrapper">
+			<br>
+			<div class="containerContent">
+			
+				<div class="content">
+
+<b>Was ist digital.hhz.de?</b>
+
+<br>
+<br>
+
+Diese Webseite zeigt verschiedene Räume des Herman Hollerith Zentrums (HHZ) in Böblingen. Jeder Raum ist mit Sensoren ausgestattet und über eine IoT-Infrastruktur vernetzt. Sensorwerte für Temperatur, Luftfeuchtigkeit, Luftqualität (CO2 Gehalt) und Helligkeit können eingesehen werden. Zusätzlich geben Detailinformationen jedes Sensors Auskunft über Änderungsaktualisierung und Durchschnittswert.
+
+<br>
+<br>
+
+<b>Warum gibt es digital.hhz.de?</b>
+
+<br>
+<br>
+
+Studierende des Studiengangs Services Computing haben verschiedene Szenarien zur digitalen Unterstützung von Forschung und Lehre am HHZ entworfen. Im Jahresprojekt Digital HHZ implementierten sie eine über mehrere Räume des HHZ verteilte IoT Sensorinfrastruktur. Sie läuft im 24/7 Betrieb und liefert verschiedene Umweltmesswerte. Eine Plattform aggregiert die Daten und ermöglicht die Entwicklung von Digital HHZ Services. Erste Services nutzen die Daten als Assistenzsystem bei der Durchführung von Lehrveranstaltungen.
+
+<br>
+<br>
+
+<b>Wofür gibt es digital.hhz.de?</b>
+
+<br>
+<br>
+
+Professoren, Studenten und Mitarbeiter des HHZ können anhand von Services und einer zusätzlich entwickelten App diese Umweltmessdaten einsehen, auswerten und für eigene Anwendungen verwenden. Einige Services sind in den Vorlesungsräumen bereits aktiv, wie beispielsweise <a href="https://github.com/digitalhhz/farbenfroh" alt="FarbenFROH GitHub page">FarbenFROH</a>. Ein weiterer Service, <a href="https://github.com/digitalhhz/stayfocussed" alt="StayFocussed GitHub page">StayFocussed</a>, verwendet die Sensorwerte und berechnet Empfehlungen für Pausen, die Verbesserung von Umweltbedingungen und die Organisation der Lehrveranstaltung.
+
+<br>
+<br>
+
+Weitere Informationen finden Sie auf unserer <a href="https://github.com/digitalhhz/digital_hhz/wiki" alt="Digital HHZ GitHub">GitHub Seite</a> 
+
+
+				</div>
+
+			</div>
+			
+		</div>
+				
+	</body>
+
+</html>
